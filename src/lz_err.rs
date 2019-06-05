0e41e767ac6933897b4ab4e53621089cdd454438
storyfeet <thebeaky@psmail.net>
2019-06-05 14:36:18 +0100

    now implements failure::Error

--- src/lz_err.rs
+++ src/lz_err.rs
@@ -1,24 +1,29 @@
+use failure_derive::*;
 
-#[derive(Debug,PartialEq)]
-pub enum LzErr{
+#[derive(Debug, PartialEq, Fail)]
+pub enum LzErr {
+    #[fail(display = "Parse Error")]
     ParseErr,
+    #[fail(display = "Parse Error at {}", 0)]
     ParseErrAt(usize),
+    #[fail(display = "Load Error")]
     LoadErr,
+    #[fail(display = "Not Found Error")]
     NotFound,
+    #[fail(display = "Env Var Error")]
     EnvVarErr,
 }
 
 use self::LzErr::*;
 
-impl From<std::io::Error> for LzErr{
-    fn from(_:std::io::Error)->Self{
+impl From<std::io::Error> for LzErr {
+    fn from(_: std::io::Error) -> Self {
         LoadErr
     }
 }
 
-impl From<std::env::VarError> for LzErr{
-    fn from(_:std::env::VarError) ->Self{EnvVarErr}
+impl From<std::env::VarError> for LzErr {
+    fn from(_: std::env::VarError) -> Self {
+        EnvVarErr
+    }
 }
-
-
-
